600e574b938226261e719322b29e558d3ce1b667
Alexey <sotnikovalexey93@mail.ru>
2023-01-17 22:00:06 +0300

    task001

---
+++ task001/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine ("Введите трехзначное число");
+string NumberStr1 = Console.ReadLine();
+int number = Convert.ToInt32 (NumberStr1);
+
+Console.WriteLine(NumberStr1[1]);
